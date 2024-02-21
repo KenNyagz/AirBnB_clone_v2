94d40c1250f62a516b7574dc7f5a8b0fe3d72cca
KenNyagz <kennyagz681@gmail.com>
2024-02-21 23:28:55 +0300

    An aliased Commit to save time

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -66,7 +66,7 @@ class DBStorage:
 
     def reload(self):
         """create all tables in the database """
-        Base.metadata.create_all(self.__engine)
+        #Base.metadata.create_all(self.__engine)
         session_db = sessionmaker(bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(session_db)
         self.__session = Session()
