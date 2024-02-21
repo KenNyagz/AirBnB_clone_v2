a1114cde61a34e2bd1ce8596bd96b561e2f4add0
ritaOmondi <cherylritaomondi@gmail.com>
2024-02-21 05:18:01 -0800

    Revert "relationships. Tasks 6-10"
    This reverts commit 213f6fac6c6c95f7be3e8b5c16454cd11bfe3f32.

--- models/amenity.py
+++ models/amenity.py
@@ -1,18 +1,7 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-import models
-from models.base_model import BaseModel, Base
-from os import getenv
-import sqlalchemy
-from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship
+from models.base_model import BaseModel
 
 
-class Amenity(BaseModel, Base):
-    """ represents Amentiy"""
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = "amenities"
-        name = Column(String(128), nullable=False)
-        # place_amenities = relationship("Place", secondary=place_amenity)
-    else:
-        name = ""
+class Amenity(BaseModel):
+    name = ""
