ba929ed20f2b53c952757429d492808827d946fe
KenNyagz <kennyagz681@gmail.com>
2024-03-07 19:26:39 +0300

    An aliased Commit to save time

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -17,16 +17,20 @@ def do_deploy(archive_path):
         return False
 
     try:
-        put(archive_path, '/tmp/')
+        put(archive_path, "/tmp/")
 
+        # Uncompress the archive to the /data/web_static/releases/ directory on the web server
         archive_filename = os.path.basename(archive_path)
-        archive_folder = "/data/web_static/releases{}".format(os.path.splitext(archive_filename)[0])
-        run('mkdir -p {}'.format(archive_folder))
-        run('tar -xzf /tmp/{} -C {}'.format(archive_filename, archive_folder))
-        run('rm /tmp/{}'.format(archive_filename))
+        archive_folder = "/data/web_static/releases/{}".format(os.path.splitext(archive_filename)[0])
+        run("mkdir -p {}".format(archive_folder))
+        run("tar -xzf /tmp/{} -C {}".format(archive_filename, archive_folder))
+        run("rm /tmp/{}".format(archive_filename))
 
-        run('rm -rf /data/web_static/current')
-        run('ln -s {} /data/web_static/current'.format(archive_folder))
+        # Delete the symbolic link /data/web_static/current from the web server
+        run("rm -rf /data/web_static/current")
+
+        # Create a new symbolic link /data/web_static/current on the web server
+        run("ln -s {} /data/web_static/current".format(archive_folder))
 
         print('Deployment successful')
         return True
