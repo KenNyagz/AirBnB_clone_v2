4340b42ec692366abfd4d3e7c2022ac257ed6843
KenNyagz <kennyagz681@gmail.com>
2024-03-07 17:45:26 +0300

    Compress before sending

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,24 +1,27 @@
-#!/usr/bin/python3
-'''
-   fabfile generates .tgz ar from web_static/'s contents
-'''
 from fabric.api import local
 from datetime import datetime
-
+import os
 
 def do_pack():
-    '''return archive path of archive created from web_static contents'''
-    try:
-        local("mkdir -p versions")
-
-        now = datetime.now()
-        archive_name = "web_static_{}{}{}{}{}{}.tgz".format(
-            now.year, now.month, now.day, now.hour, now.minute, now.second)
-
-        # Create the .tgz archive
-        local("tar -czvf versions/{} web_static".format(archive_name))
-
-        return "versions/{}".format(archive_name)
-    except Exception as e:
-        print("Couldn't create archive")
+    # Create the directory versions if it doesn't exist
+    if not os.path.isdir("versions"):
+        os.mkdir("versions")
+    
+    # Format the current datetime to create a unique filename
+    now = datetime.now()
+    filename = "web_static_{}{}{}{}{}{}.tgz".format(now.year, now.month, now.day, now.hour, now.minute, now.second)
+    
+    # The full path of the archive to be created
+    archive_path = "versions/{}".format(filename)
+    
+    # Command to tar and compress the contents of web_static
+    command = "tar -cvzf {} web_static".format(archive_path)
+    
+    print("Packing web_static to {}".format(archive_path))
+    # execute command
+    result = local(command, capture=True)
+    
+    if result.failed:
         return None
+    else:
+        return archive_path
