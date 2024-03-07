33c5ed7783ec3aa6f88ef354870e459b804ae193
KenNyagz <kennyagz681@gmail.com>
2024-03-07 16:44:35 +0300

    Compress before sending

---
+++ 1-pack_web_static.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# fabfile generates .tgz ar from web_static/'s contents
+from fabric.api import local
+from datetime import datetime
+
+
+def do_pack:
+    '''return archive path of archive created from web_static contents'''
+    try:
+        local("mkdir -p versions")
+
+        now = datetime.now()
+        archive_name = "web_static_{}{}{}{}{}{}.tgz".format(
+            now.year, now.month, now.day, now.hour, now.minute, now.second)
+
+        # Create the .tgz archive
+        local("tar -czvf versions/{} web_static".format(archive_name))
+
+        return "versions/{}".format(archive_name)
+    except Exception as e:
+        print("Couldn't create archive")
+        return None
