99c72f5731ef4160033aa87685262e23cd3e7839
KenNyagz <kennyagz681@gmail.com>
2024-03-08 00:52:16 +0300

    An aliased Commit to save time

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,6 +1,6 @@
-# Puppet manifest that sets up web-servers for deployment
+# Configures a web server for deployment of web_static.
 
-#Nginx configuration file
+# Nginx configuration file
 $nginx_conf = "server {
     listen 80 default_server;
     listen [::]:80 default_server;
@@ -27,55 +27,61 @@ package { 'nginx':
 } ->
 
 file { '/data':
-  ensure => 'directory',
-}->
+  ensure  => 'directory'
+} ->
 
 file { '/data/web_static':
   ensure => 'directory'
-}
+} ->
 
 file { '/data/web_static/releases':
   ensure => 'directory'
-}->
+} ->
 
 file { '/data/web_static/releases/test':
   ensure => 'directory'
-}->
+} ->
 
 file { '/data/web_static/shared':
   ensure => 'directory'
-}->
+} ->
 
-file { '/data/web_static/test/index.html':
+file { '/data/web_static/releases/test/index.html':
   ensure  => 'present',
-  content => 'Holberton School Puppet\n',
-}->
+  content => "Holberton School Puppet\n"
+} ->
 
 file { '/data/web_static/current':
-  ensure => 'link'
+  ensure => 'link',
   target => '/data/web_static/releases/test'
-}->
+} ->
 
-file { 'chown -R ubuntu"ubuntu /data/':
+exec { 'chown -R ubuntu:ubuntu /data/':
   path => '/usr/bin/:/usr/local/bin/:/bin/'
-}->
+}
 
-file { '/var/www/':
+file { '/var/www':
   ensure => 'directory'
-}->
+} ->
 
 file { '/var/www/html':
   ensure => 'directory'
-}->
+} ->
 
-file { '/var/www/404.html':
-  ensure => 'present'
-}->
+file { '/var/www/html/index.html':
+  ensure  => 'present',
+  content => "Holberton School Nginx\n"
+} ->
 
-file { '/etc/nginx/site-available/default':
+file { '/var/www/html/404.html':
+  ensure  => 'present',
+  content => "Ceci n'est pas une page\n"
+} ->
+
+file { '/etc/nginx/sites-available/default':
   ensure  => 'present',
   content => $nginx_conf
-}->
+} ->
 
 exec { 'nginx restart':
   path => '/etc/init.d/'
