107a5d74b3c40a137ed370bd0e4b1b47ff26d71b
KenNyagz <kennyagz681@gmail.com>
2024-03-09 00:10:50 +0300

    Deploy archive

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,14 +5,14 @@ from os.path import exists
 from datetime import datetime
 
 
-env.hosts = ['54.236.17.14']
+env.hosts = ['54.236.17.14', '18.209.223.91']
 env.user = 'ubuntu'
 env.key_file = '~/.ssh/id_rsa'
 
 
 def do_deploy(archive_path):
     '''deploys static content to web servers'''
-    if not exists:
+    if not exists(archive_path) :
         return False
 
     try:
@@ -21,16 +21,20 @@ def do_deploy(archive_path):
         archive_filename = archive_path.split('/')[-1]
         archive_name = archive_filename.split('.')[0]
         release_path = '/data/web_static/releases/{}/'.format(archive_name)
-        run('mkdir -p {}'.format(release_path))
-        run('tar -xzf /tmp/{} -C {}'.format(archive_filename, release_path))
+        sudo('mkdir -p {}'.format(release_path))
+        sudo('tar -xzf /tmp/{} -C {}'.format(archive_filename, release_path))
+        sudo('mv /data/web_static/releases/{}/web_static/*
+ /data/web_static/releases/{}/'.format(archive_name, archive_name))
+        sudo('rm -rf /data/web_static/releases/{}/web_static'.format(
+            archive_name))
 
         # Delete archive from archive
-        run('rm /tmp/{}'.format(archive_filename))
+        sudo('rm /tmp/{}'.format(archive_filename))
 
         # Delete existing symbolic link
-        run('rm -rf /data/web_static/current')
+        sudo('rm -rf /data/web_static/current')
         # new symbolic link
-        run('ln -s {} /data/web_static/current'.format(release_path))
+        sudo('ln -s {} /data/web_static/current'.format(release_path))
 
         print('Deployment successful')
         return True
