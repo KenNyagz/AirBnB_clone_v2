2709ac452da12b8176ea62deab4a334bee21e643
KenNyagz <kennyagz681@gmail.com>
2024-03-09 15:52:07 +0300

    An aliased Commit to save time

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -27,39 +27,39 @@ def do_pack():
         print("Couldn't create archive")
         return None
 
-'''
-def do_deploy(archive_path):
-    '''deploys static content to web servers'''
-    if not exists:
-        return False
 
-    try:
-        put(archive_path, '/tmp/')
+#def do_deploy(archive_path):
+ #   '''deploys static content to web servers'''
+#    if not exists:
+#        return False
+#
+#    try:
+#        put(archive_path, '/tmp/')
 
-        archive_filename = archive_path.split('/')[-1]
-        archive_name = archive_filename.split('.')[0]
-        release_path = '/data/web_static/releases/{}/'.format(archive_name)
-        sudo('mkdir -p {}'.format(release_path))
-        sudo('tar -xzf /tmp/{} -C {}'.format(archive_filename, release_path))
-        sudo('mv /data/web_static/releases/{}/web_static/*\
- /data/web_static/releases/{}/'.format(archive_name, archive_name))
-        sudo('rm -rf /data/web_static/releases/{}/web_static'.format(
-            archive_name))
+ #       archive_filename = archive_path.split('/')[-1]
+#        archive_name = archive_filename.split('.')[0]
+#        release_path = '/data/web_static/releases/{}/'.format(archive_name)
+#        sudo('mkdir -p {}'.format(release_path))
+#        sudo('tar -xzf /tmp/{} -C {}'.format(archive_filename, release_path))
+#        sudo('mv /data/web_static/releases/{}/web_static/*\
+# /data/web_static/releases/{}/'.format(archive_name, archive_name))
+ #       sudo('rm -rf /data/web_static/releases/{}/web_static'.format(
+  #          archive_name))
 
         # Delete archive from archive
-        sudo('rm /tmp/{}'.format(archive_filename))
+   #     sudo('rm /tmp/{}'.format(archive_filename))
 
         # Delete existing symbolic link
-        sudo('rm -rf /data/web_static/current')
+    #    sudo('rm -rf /data/web_static/current')
         # new symbolic link
-        sudo('ln -s {} /data/web_static/current'.format(release_path))
+     #   sudo('ln -s {} /data/web_static/current'.format(release_path))
 
-        print('Deployment successful')
-        return True
-    except Exception as e:
-        print("Deployment failure:", e)
-        return False
-'''
+      #  print('Deployment successful')
+  #      return True
+   # except Exception as e:
+    #    print("Deployment failure:", e)
+     #   return False
+
 
 def do_deploy(archive_path):
     """distributes an archive to the web servers"""
