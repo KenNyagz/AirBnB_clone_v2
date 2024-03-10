fbd0abfd3472d22075dca62942aec0d0c680b12b
KenNyagz <kennyagz681@gmail.com>
2024-03-10 23:19:42 +0300

    Removing unwanted archives from server

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,1 +1,23 @@
+#!/usr/bin/python3
+# Clean unwanted archives from the server
+from fabric.api import env, run, local
+from fabric.operations import sudo
+from datetime import datetime
 
+
+env.hosts = ['54.236.17.14', '18.209.223.91']
+env.user = 'ubuntu'
+env.key_filename = '~/.ssh/id_rsa'
+
+
+def do_clean(number=0):
+    '''Deletes unused archive from the server'''
+    try:
+        local('ls -t versions | tail -n +{} | xargs -I {{}} rm versions/{{}}'
+              .format(number + 1))
+        for host in env.hosts:
+            releases = run("ls -t /data/web_static/releases".split())
+            for releases in releases[numbers:]:
+                sudo("rm -rf /data/web_static/releases/{}".format(releases))
+    except Exception as e:
+        print("Error")
