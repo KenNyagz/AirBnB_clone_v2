440a9d0b685a8fd4be1ede3aaefd94601ccd2613
KenNyagz <kennyagz681@gmail.com>
2024-03-23 10:42:43 +0300

    Flask routing variables

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+'''starts a Flask web application
+'''
+
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    '''Defines landing page for root dir of web app'''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    '''Defines another landing page to different path'''
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def fun_is_c(text):
+    '''Creates HTML http response for path taking argument'''
+    text = text.replace('_', ' ')
+    return f"C {text}"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
