fcd7e4af7ac82e38f3f03494d8f2b602744e223a
KenNyagz <kennyagz681@gmail.com>
2024-03-23 10:17:05 +0300

    Run FLask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -11,9 +11,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-	'''Defines landing page'''
-	return 'Hello HBNB!'
+    '''Defines landing page'''
+    return 'Hello HBNB!'
 
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
