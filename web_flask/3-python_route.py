0240dafb3dcd74a8d92b0d4877489d6765fa344e
KenNyagz <kennyagz681@gmail.com>
2024-03-23 10:50:53 +0300

    flaska

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+'''starts a Flask web application
+'''
+
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    '''Defines landing page for root dir of web app'''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    '''Defines another landing page to different path'''
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def fun_is_c(text):
+    '''Creates HTML http response for path taking argument'''
+    text = text.replace('_', ' ')
+    return f"C {text}"
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def cool_is_py(text):
+    '''Creates HTML http response for path taking argument'''
+    text = text.replace('_', ' ')
+    return f"Python {text}"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
