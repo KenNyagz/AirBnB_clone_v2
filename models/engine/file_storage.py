a13720ef218788cd852c3dc3bc5aef797afe09ea
KenNyagz <kennyagz681@gmail.com>
2024-03-24 00:18:55 +0300

    An aliased Commit to save time

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -59,3 +59,7 @@ class FileStorage:
             key = f"{obj.__class__.__name__}.{obj.id}"
             if key in FileStorage.__objects:
                 del FileStorage.__objects[key]
+
+    def close(self):
+        """ deserializing the JSON file to objects"""
+        FileStorage.reload()
